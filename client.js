c354ab6add47845c54294c67b126e6eb404226be
Mustafa <mustafa31112@gmail.com>
2022-01-20 18:39:30 +0000

    added inistials to game

--- client.js
+++ client.js
@@ -1,12 +1,13 @@
 const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243', // IP address here,
+    host: 'localhost', // IP address here,
     port: 50541 // PORT number here,
   });
   conn.on("connect", () => {
     // code that does something
     console.log("Successfully Connected")
+    conn.write("Name: Mus")
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
