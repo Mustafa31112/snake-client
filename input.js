1acaa19a0429bb45dd447ef0292710f5c98907e7
Mustafa <mustafa31112@gmail.com>
2022-01-20 20:08:37 +0000

    added movements and game is working

--- input.js
+++ input.js
@@ -1,18 +1,46 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+
+    // console.log(connection)
+    if (data === 'w') {
+      connection.write('Move: up');
+      console.log('Move: up');
+    }
+    if (data === 'a') {
+      connection.write('Move: left');
+      console.log('Move: left');
+    }
+    if (data === 'd') {
+      connection.write('Move: right');
+      console.log('Move: right');
+    }
+    if (data === 's') {
+      connection.write('Move: down');
+      console.log('Move: down');
+    }
+    if (data === 'l') {
+      connection.write('Say: wee');
+    console.log('done');  
+    }
+  };
   stdin.on("data", handleUserInput);
   return stdin;
 };
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-  
-};
-setupInput();
 
-module.exports = { setupInput};
+
+// console.log(connect())
+// setupInput()
+// 
+
+module.exports = { setupInput };
