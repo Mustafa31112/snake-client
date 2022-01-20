4d348009d109c9ccf96fa6df14dbf622a4289825
Mustafa <mustafa31112@gmail.com>
2022-01-20 19:56:35 +0000

    made a new file and imported the function

--- play.js
+++ play.js
@@ -1,24 +1,10 @@
 
 
 // establishes a connection with the game server
-const connect = require('./client');
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-  
-};
 setupInput();
